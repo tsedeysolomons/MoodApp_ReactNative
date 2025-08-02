d05a551389ae3b443180a4bd6010f5048f760254
tsedey <167250386+tsedeysolomons@users.noreply.github.com>
2025-08-02 22:23:45 +0300

    feat: implement HomeScreen component with background image and text display

---
+++ app/home/index.tsx
@@ -0,0 +1,44 @@
+//import Header from "@/app/components/header";
+// eslint-disable-next-line import/no-unresolved
+//import BgImage from ".../../assets/images/mood.png";
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import Footers from "../components/Footer";
+import Headers from "../components/header";
+import BgImage from "../../assets/images/mood.png";
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={BgImage} resizeMode="cover" style={styles.image}>
+        <Text style={styles.text}>Wellcome To check your current Emotion!</Text>
+        <Text style={styles.text}>ስለዚ ለመለወት ካሰባችሁ ነገም ለላ ከን ነው፡፡</Text>
+      </ImageBackground>
+      <Footers />
+    </View>
+  );
+};
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    borderCurve: "continuous",
+  },
+  text: {
+    fontFamily: "check",
+    color: "white",
+    fontSize: 24,
+    justifyContent: "center",
+  },
+  Button: {
+    color: "rgba(0,0,0,0.25)",
+  },
+});
